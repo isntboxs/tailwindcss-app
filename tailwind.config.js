eb84fdba66293690349b4071fa49d5a5d4e85d72
mrboxs <rizkywanda200155@gmail.com>
2024-05-26 03:59:09 +0700

    hero section, about section dan navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#6d6d6d",
+        black: "#1c1c1c",
+        woodsmoke: {
+          50: "#f6f6f6",
+          100: "#e7e7e7",
+          200: "#d1d1d1",
+          300: "#b0b0b0",
+          400: "#888888",
+          500: "#6d6d6d",
+          600: "#5d5d5d",
+          700: "#4f4f4f",
+          800: "#454545",
+          900: "#3d3d3d",
+          950: "#1c1c1c",
+        },
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
